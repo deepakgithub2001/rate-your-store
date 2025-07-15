83b12796377ad034cccf61aca8a8095f58c50e79
Deepak Kumar <deepakchaudhary.cse2001@gmail.com>
2025-07-15 08:11:23 +0000

    update the footer

--- rate-your-store-client/src/components/Footer.jsx
+++ rate-your-store-client/src/components/Footer.jsx
@@ -1,7 +1,6 @@
-// src/components/Footer.jsx
 const Footer = () => {
   return (
-    <footer className="w-full bg-slate-900 border-t py-4 text-center text-white fixed bottom-0">
+    <footer className="w-full bg-slate-900 border-t py-4 text-center text-white mt-auto">
       © {new Date().getFullYear()} Rate Your Store. All rights reserved.
     </footer>
   );
